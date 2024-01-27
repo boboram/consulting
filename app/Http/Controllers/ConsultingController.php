a9fcd97c3534aed499dfc6242cb2a08649244555
boboram <5018b@naver.com>
2024-01-27 14:47:57 +0900

    controller 작업(ConsultingController)

---
+++ app/Http/Controllers/ConsultingController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Data\Consulting\ConsultingData;
+use App\Http\Requests\Consulting\ConsultingRequest;
+use App\Http\Resources\Consulting\Consulting;
+use App\Http\Resources\Consulting\ConsultingCollection;
+use App\Http\Services\PersonalTraining;
+use Illuminate\Http\Response;
+
+/**
+ * Class ConsultingController
+ * @package App\Http\Controllers
+ */
+class ConsultingController extends Controller
+{
+
+    /**
+     * @var \App\Http\Services\PersonalTraining
+     */
+    private PersonalTraining $personalTraining;
+
+    /**
+     * ConsultingController constructor.
+     * @param \App\Http\Services\PersonalTraining $personalTraining
+     */
+    public function __construct(PersonalTraining  $personalTraining)
+    {
+        $this->personalTraining = $personalTraining;
+    }
+
+    /**
+     * @param \App\Http\Requests\Consulting\ConsultingRequest $request
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    protected function consulting(ConsultingRequest $request): Response
+    {
+        $data = ConsultingData::from($request->all());
+
+        $result = $this->personalTraining->consulting($data);
+
+        return response()->success(new ConsultingCollection($result), Response::HTTP_OK);
+    }
+}
